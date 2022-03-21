fe3b6ea8060325af478eb192645465a471bc3d5f
Karl Stencell <karldstencell@gmail.com>
2022-03-21 19:00:58 -0400

    added dynamic layout to login page

--- Server/Source.cpp
+++ Server/Source.cpp
@@ -7,6 +7,17 @@ using namespace std;
 
 int main() {
 	Account_DB_Handler adb;
-	Account acc = adb.login();
+	
+	string u = "hi";
+
+	Account a = new Account(1, u, u,  u, u, 1, 2, 1, true);
+
+	AccountPacket np(&a);
+
+	np.serializeAccountPacketTxBuffer();
+
+	char* m = np.getSerializedAccountPacketBuffer();
+
+	AccountPacket r(m);
 
 }
