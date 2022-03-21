a9e1cab1c6852538d9aed44f26d37a9e472c0589
Thomas Horvath <Thorvath6447@conestogac.on.ca>
2022-03-21 14:30:56 -0400

    Began work on the game

--- Server/GameBoard.h
+++ Server/GameBoard.h
@@ -1,9 +1,12 @@
 #pragma once
+#include <random>
 #include "GameRoom.h"
 
 class GameBoard
 {
-	int board[3][3]; // represents the 3 by 3 board of tic tac toe
+	char board[3][3] = { {'1','2','3'}, 
+						 {'4','5','6'}, 
+					     {'7','8','9'} }; // represents the 3 by 3 board of tic tac toe
 	
 	enum Status // temp enum placement
 	{
@@ -16,27 +19,81 @@ public:
 
 	GameBoard()
 	{
-
+					
 	}
 
-	void placePlayerMove(Player p, int move)
+	void placePlayerMove(int move)
 	{
-
+		while(!validateMove(move,1))
+		{
+			ifEnd();
+			// some sort of confirmation move was played and initiation of response to the player
+		}
 	}
 
 	void placeComputerMove()
 	{
+		int temp = rand() % 9;
+
+		while (!validateMove(temp, 0))
+		{
+			ifEnd();
+			// some sort of confirmation move was played and initiation of response to the player
+		}
 
 	}
 
 	void ifEnd()
 	{
+		for (int i = 0; i <= 3, i++;)
+		{
+			if (board[i][0] == board[i][1] && board[i][1] == board[i][2])
+			{
+			
+			}
+			for (int j = 0; j <= 3, j++;)
+			{
+				if (board[i][j] == board[i][j] && board[i][j] == board[i][j])
+				{
 
+				}
+			}
+		}
 	}
 
-	void validateMove(int move)
-	{
+	bool validateMove(int move, int player)
+	{	
+		int col = 0;
+		int row = 0;
+		if (move <= 3)
+		{
+			col = move - 1;
+		}
+		else if (move <= 6)
+		{
+			row = 1;
+			col = move - 4;
+		}
+		else if (move <= 9)
+		{
+			row = 2;
+			col = move - 7;
+		}
 
+		if (board[row][col] == (char)move)
+		{
+			if (player == 0)
+			{
+				board[row][col] = 'x';
+			}
+			else if (player == 1)
+			{
+				board[row][col] = '0';
+			}
+			return true;
+		}
+		else
+			return false;
 	}
 
 	Status getStatus()
--- Server/Source.cpp
+++ Server/Source.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include "Account.h"
 #include "Account_DB_Handler.h"
+#include "GameBoard.h"
+#include "GameRoom.h"
 
 using namespace std;
 
 
 int main() {
 	Account_DB_Handler adb;
-	Account acc = adb.login();
 
 }
