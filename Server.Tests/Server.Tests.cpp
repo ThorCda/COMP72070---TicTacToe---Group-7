0fcfadc9b416b990cd7d97c6d8c18454bc88568f
Gavin Abeele <abeele.s.gavin@gmail.com>
2022-03-23 14:31:47 -0400

    Account packet Testing.

--- Server.Tests/Server.Tests.cpp
+++ Server.Tests/Server.Tests.cpp
@@ -10,8 +10,8 @@ namespace ServerTests_Packet
 	TEST_CLASS(ServerTests)
 	{
 	public:
-		
-		TEST_METHOD(PKT_UNT_016_Create_Account_Packet)
+
+		TEST_METHOD(Create_Account_Packet)	//PKT_UNT_016 through 020
 		{
 			bool isSame = true;
 
@@ -33,20 +33,72 @@ namespace ServerTests_Packet
 			if (strcmp(np->getFName(), r->getFName()) != 0) {
 				isSame = false;
 			}
-			if (strcmp(np->getFName(), r->getFName()) != 0) {
+			if (strcmp(np->getLName(), r->getLName()) != 0) {
 				isSame = false;
 			}
-			if (strcmp(np->getFName(), r->getFName()) != 0) {
+			if (strcmp(np->getPassword(), r->getPassword()) != 0) {
 				isSame = false;
 			}
-			if (strcmp(np->getFName(), r->getFName()) != 0) {
+			if (strcmp(np->getUsername(), r->getUsername()) != 0) {
 				isSame = false;
 			}
-			
 
 			Assert::IsTrue(isSame);
-			
 
 		}
+
+		TEST_METHOD(Account_Packet)	//PKT-UNT-016 through 020
+
+		{
+			bool isSame = true;
+
+			char us[] = "Gabeele";
+			char fis[] = "Gavin";
+			char la[] = "Abeele";
+			char pic[] = "C://Hello.png";
+
+			Account a = new Account(1, fis, la, pic, us, 1, 2, 1, true);
+
+			AccountPacket np(&a);
+
+			np.serializeAccountPacketTxBuffer();
+
+			char* m = np.getSerializedAccountPacketBuffer();
+
+			AccountPacket* r = new AccountPacket(m);
+
+			Account* actual = np.getAccount();
+			Account* result = r->getAccount();
+
+			if (actual->getAccountID() == result->getAccountID()) {
+				isSame = false;
+			}
+			if (strcmp(actual->getUserName(), result->getUserName()) != 0) {
+				isSame = false;
+			}
+			if (strcmp(actual->getFirstName(), result->getFirstName()) != 0) {
+				isSame = false;
+			}
+			if (strcmp(actual->getLastName(), result->getLastName()) != 0) {
+				isSame = false;
+			}
+			if (strcmp(actual->getAvatarloc(), result->getAvatarloc()) != 0) {
+				isSame = false;
+			}
+			if (actual->getWins() == result->getWins()) {
+				isSame = false;
+			}
+			if (actual->getLoses() == result->getLoses()) {
+				isSame = false;
+			}
+			if (actual->getDraws() == result->getDraws()) {
+				isSame = false;
+			}
+
+			Assert::IsTrue(isSame);
+
+		}
+
+
 	};
 }
