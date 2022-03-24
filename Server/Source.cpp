b9b353f5b6a770d2bdd46df0c4e934f9549b5aa6
Gavin Abeele <abeele.s.gavin@gmail.com>
2022-03-24 14:01:14 -0400

    Clean up

--- Server/Source.cpp
+++ Server/Source.cpp
@@ -9,47 +9,4 @@ using namespace std;
 
 int main() {
 	
-
-
-	//char we[] = "Gavin";
-	//char p[] = "Conestoga";
-
-	//LoginPacket* l = new LoginPacket(we, p);
-
-	//l->serilizeLoginPacket();
-
-	//char* o = l->getSerializedTxBuffer();
-
-	//LoginPacket* t = new LoginPacket(o);
-
-	//LogoutPacket* j = new LogoutPacket(false);
-
-	//j->serilizeLogoutPacket();
-
-	//char* v = j->getSerializedTxBuffer();
-
-	//LogoutPacket* q = new LogoutPacket(v);
-
-	/*MovePacket* x = new MovePacket(3);
-
-	x->serializeMovePacketTxBuffer();
-
-	char* z = x->getSerializedTxBuffer();
-
-	MovePacket* u = new MovePacket(z);*/
-	
-	/*int gameStatus = 3;
-
-	GameStatusPacket* xx = new GameStatusPacket(gameStatus);
-
-	xx->serializeGameStatusPacketBuffer();
-
-	char* zz = xx->getSerializedTxBuffer();
-
-	GameStatusPacket* xxx = new GameStatusPacket(zz);
-
-	int x = 0;*/
-
-
-
 }
