285733a205b29f0b6159f2a53360dd6140e71315
Gavin Abeele <abeele.s.gavin@gmail.com>
2022-03-29 15:06:19 -0400

    Moving Enums to the handler.

--- Server/Logs.h
+++ Server/Logs.h
@@ -4,25 +4,7 @@
 #include <string>
 #include <ctime>
 
-enum ACTION_TYPE
-{
-    type_not_defined, buf_send, buf_receive, connected, disconnected
-};
 
-enum MATCH_STATUS
-{
-     status_not_defined, playing, ended
-};
-
-enum FILE_TYPE
-{
-    error_log, game_log, conn_log
-};
-
-enum ERROR_CODE	
-{
-    Login_Err, CrtAcc_Err, Quit_Err, Conn_Err, Move_Err
-};
  class Logs {
 
       static const char* error_file;
--- Server/NetworkHandler.h
+++ Server/NetworkHandler.h
@@ -9,6 +9,26 @@
 
 using namespace std;
 
+enum ACTION_TYPE
+{
+	type_not_defined, buf_send, buf_receive, connected, disconnected
+};
+
+enum MATCH_STATUS
+{
+	status_not_defined, playing, ended
+};
+
+enum FILE_TYPE
+{
+	error_log, game_log, conn_log
+};
+
+enum ERROR_CODE
+{
+	Login_Err, CrtAcc_Err, Quit_Err, Conn_Err, Move_Err
+};
+
 class NetworkHandler
 {
 	SOCKET ListenSocket;
